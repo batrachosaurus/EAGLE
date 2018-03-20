bb6ce4e0641b78b6a8e5984394e2bf644fb5784a
loven-doo <moshenskydenis@gmail.com>
2018-03-20 15:28:40 +0300

    families preparing is established

--- EAGLEdb/constants.py
+++ EAGLEdb/constants.py
@@ -4,3 +4,4 @@ constants_path = os.path.dirname(os.path.realpath(__file__))
 
 bacteria_list_f_name = "bacteria.json"
 analyzed_bacteria_f_name = "analyzed_bacteria.p"
+bact_fam_f_name = "bact_fam.json"
