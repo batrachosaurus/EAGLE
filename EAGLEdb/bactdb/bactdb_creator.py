aa86cf6369a1b12e4f1e318c4056e4ecaf65059b
loven-doo <moshenskydenis@gmail.com>
2018-05-01 18:41:05 +0300

    seq ids reduction upgraded

--- EAGLEdb/bactdb/bactdb_creator.py
+++ EAGLEdb/bactdb/bactdb_creator.py
@@ -343,24 +343,29 @@ def prepare_family(family_name, family_data, db_dir):
             for strain in family_data[genus][species].keys():
                 bacterium_rRNA_dict = load_fasta_to_dict(family_data[genus][species][strain]["16S_rRNA_file"])
                 for rRNA_id in bacterium_rRNA_dict.keys():
-                    ids_to_org_dict[rRNA_id] = strain
-                rRNA_seqs_dict.update(bacterium_rRNA_dict)
+                    new_rRNA_id = None
+                    new_rRNA_id = rRNA_id.split(" ")[0].split("|")[1]
+                    ids_to_org_dict[new_rRNA_id] = strain
+                    rRNA_seqs_dict[new_rRNA_id] = bacterium_rRNA_dict[rRNA_id]
+
+    # reduce and modify ids
     # TODO: follows
     ### This section will be upgraded with my own alignment method but now MUSCLE and hmmer 3 are used
+    tmp_fam_dir = os.path.join(db_dir, family_name+"_tmp")
     rRNA_aln = construct_mult_aln(seq_dict=rRNA_seqs_dict,
                                   method="MUSCLE",
                                   aln_type="nucl",
                                   aligner_inst_dir=conf_constants.muscle_inst_dir,
                                   hmmer_inst_dir=conf_constants.hmmer_inst_dir,
-                                  tmp_dir=os.path.join(db_dir, family_name+"_tmp"),
+                                  tmp_dir=tmp_fam_dir,
                                   logger=EAGLE_logger)
     rRNA_aln.remove_paralogs(ids_to_org_dict, method="min_dist", inplace=True)  # If I use my own alignment method: method="spec_pos"
     family_data["16S_rRNA_gtf"] = os.path.join(db_dir, family_name+"_16S_rRNA.gtf")
     family_data["16S_rRNA_fasta"] = os.path.join(db_dir, family_name+"_16S_rRNA.fasta")
-    rRNA_aln.to_gtf(gtf_path=family_data["16S_rRNA_gtf"],
-                    fasta_path=family_data["16S_rRNA_fasta"],
-                    meta_dict=ids_to_org_dict)
-    family_data["16S_rRNA_tree"] = build_tree_by_dist(rRNA_aln.get_distance_matrix())
+    rRNA_aln.get_blocks_tsv(gtf_path=family_data["16S_rRNA_gtf"],
+                            fasta_path=family_data["16S_rRNA_fasta"],
+                            meta_dict=ids_to_org_dict)
+    family_data["16S_rRNA_tree"] = build_tree_by_dist(rRNA_aln.get_distance_matrix(), tmp_dir=tmp_fam_dir).newick()
     family_data["16S_rRNA_profile"] = os.path.join(db_dir, family_name+".hmm")
     # profiles should not be here
     rRNA_aln.get_hmm_profile(method='hmmer', profile_path=family_data["16S_rRNA_profile"])  # hmmer will be replaced with my own method
