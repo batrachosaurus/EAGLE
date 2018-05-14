4dbec4e50b304f8a2b07cd7b173317177c5c34e2
dmm <moshenskydenis@gmail.com>
2018-05-15 00:24:56 +0300

    RedisQueue.get(block=False) testing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='EAGLE',
-      version='0.8.1',
+      version='0.8.2',
       packages=find_packages(),
       package_data={'EAGLE': ['configs/*', ],
                     'EAGLEdb': ['org_tables/*', ]},
