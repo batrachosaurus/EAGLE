41fb8b98654f8885920a7740b2fe3943c7809d4f
dmm <moshenskydenis@gmail.com>
2018-05-17 03:07:31 +0300

    get_families_dict debugging paused on remove_paralogs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='EAGLE',
-      version='0.9.6',
+      version='0.9.7',
       packages=find_packages(),
       package_data={'EAGLE': ['configs/*', ],
                     'EAGLEdb': ['org_tables/*', ]},
