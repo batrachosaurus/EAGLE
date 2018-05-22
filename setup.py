1978c2ffc51012ad698b8004ab91e92a477a5cb2
dmm <moshenskydenis@gmail.com>
2018-05-23 00:25:26 +0300

    shutil bug fixed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='EAGLE',
-      version='0.12.3',
+      version='0.12.4',
       packages=find_packages(),
       package_data={'EAGLE': ['configs/*', ],
                     'EAGLEdb': ['org_tables/*', ]},
