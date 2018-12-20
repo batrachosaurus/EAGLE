fbd7a13d9982f415323f471db0b218eea75b6e96
loven-doo <1429305+loven-doo@users.noreply.github.com>
2018-12-20 19:21:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='EAGLE',
-      version='0.19.4',
+      version='0.20.0',
       author="Denis Moshensky",
       author_email="loven-doo@fbb.msu.ru", 
       description="Essential and Advantageous Genes Location Explorer",
