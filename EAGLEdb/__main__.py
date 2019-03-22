21ead2c5ea9617ec46c31fffcd948d745287f960
dmm <moshenskydenis@gmail.com>
2019-03-23 00:21:44 +0300

    EAGLEdb run scheme upgraded

--- EAGLEdb/__main__.py
+++ EAGLEdb/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 
-from EAGLE.constants import conf_constants
+from EAGLE.constants import conf_constants, EAGLE_logger
 from EAGLEdb import create_bactdb
 from EAGLEdb.constants import conf_constants_db
 
@@ -21,6 +21,23 @@ def _parse_cmd_args(*args):
     parser.add_argument("-igenbank",
                         "--input-table-genbank",
                         help="Path to a table with organisms to download from NCBI genbank",
+                        required=False,
+                        default=None)
+    parser.add_argument("-icustom",
+                        "--input-table-custom",
+                        help="Path to a table with custom genomes and their taxonomy (not implemented yet)",
+                        required=False,
+                        default=None)
+    parser.add_argument("-btcp",
+                        "--btax-class-profile",
+                        help="The path to HMM profile of sequences that should be used for base taxons classification "
+                             "while the db construction (not implemented yet)",
+                        required=False,
+                        default=None)
+    parser.add_argument("-btrp",
+                        "--btax-rep-profile",
+                        help="The path to HMM profile of sequences that should be used for a base taxon response "
+                             "while essential and advantageous genes exploration (not implemented yet)",
                         required=False,
                         default=None)
     parser.add_argument("-d",
@@ -61,7 +78,19 @@ def _parse_cmd_args(*args):
 def main():
     args_dict = _parse_cmd_args(*sys.argv[1:])
     if args_dict["dbtype"].lower() in ("bacteria", "bact", "b"):
+        EAGLE_logger.info("'Bacteria' mode selected")
         create_bactdb(**args_dict)
+    # metagenomes mode
+    if args_dict["dbtype"].lower() in ("eukaryota", "eukaryot", "eukar", "euk", "eu", "e"):
+        print("'Eukaryota' mode is not implemented yet")
+    # mito mode
+    if args_dict["dbtype"].lower() in ("other", "o"):
+        print("'Other' mode is not implemented yet")
+        if not args_dict["input_table_custom"] or not args_dict["btax_class_profile"]:
+            print("'input-table-custom' and 'btax-class-profile' parameters required for 'Other' mode")
+            return
+    else:
+        print("Unsupported mode '%s'" % args_dict["dbtype"])
 
 
 if __name__ == "__main__":
