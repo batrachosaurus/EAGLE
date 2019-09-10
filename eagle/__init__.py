3b540c71fede07b14c9d22a649b99643d65e14ec
Denis <loven7doo@gmail.com>
2019-09-10 14:54:41 +0300

    imports bug fixed

--- eagle/__init__.py
+++ eagle/__init__.py
@@ -1,3 +1,3 @@
-from orfs_explorer import explore_orfs
-from orfs_classifier import classify_orfs
-from btax_scanner import get_btax_name
+from eagle.orfs_explorer import explore_orfs
+from eagle.orfs_classifier import classify_orfs
+from eagle.btax_scanner import get_btax_name
