2183fb831ecb620c3fc1fc264d5ef8a670295bb5
Denis <loven7doo@gmail.com>
2021-09-19 00:01:53 +0300

    eagledb.bacteria.scenarios

--- eagledb/bacteria/scenarios.py
+++ eagledb/bacteria/scenarios.py
@@ -1,22 +1,31 @@
 import os
 import argparse
 
+from eagledb.creation import create
 
-BACTERIA_REFSEQ_SUMMARY_LINK = "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt"
-BACTERIA_GENBANK_SUMMARY_LINK = "https://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt"
+
+BACTERIA_SUMMARY_LINKS = {
+    "refseq": "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt",
+    "genbank": "https://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt"
+}
 DB_DIR = os.path.join("EAGLEdb", "bacteria")
 # TODO: add reference profiles
 
 
-def create_refseq_cmd():
+def create_ncbi_cmd():
     parser = argparse.ArgumentParser()
     pass
 
 
-def create_refseq(db_dir=None, num_threads=None, config_path=None):
+def create_ncbi(db_name: str, db_dir=None, num_threads=None, config_path=None):
     # conf_constants update
 
-    # download BACTERIA_REFSEQ_SUMMARY_LINK
-    # prepare BACTERIA_REFSEQ_SUMMARY
-    # create_ncbi(...)
+    bacteria_summary_link = BACTERIA_SUMMARY_LINKS[db_name.lower()]
+    # download BACTERIA_SUMMARY_LINK
+    # prepare BACTERIA_SUMMARY
+    # create(...)
+    pass
+
+
+def collect_orgs_ncbi(orgs_table, db_dir=None):
     pass
