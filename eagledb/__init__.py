f190e6fb233654b95b3c536be2e96f82514a8204
Denis <loven7doo@gmail.com>
2021-09-18 21:28:17 +0300

    eagledb.creation

--- eagledb/__init__.py
+++ eagledb/__init__.py
@@ -1,2 +1,4 @@
 from eagledb.files_utils import are_bacteria_analyzed, prepare_summary_table, join_genomes_lists
 from eagledb.bactdb_creation import get_bacteria_from_ncbi, get_families_dict, create_bactdb
+
+from eagledb.creation import create
---
+++ eagledb/creation.py
@@ -0,0 +1,20 @@
+# each path for DB file should be stored as relative path from db_dir
+# each time a DB file is used it should be accessed with os.path.join(db_dir, relative_f_path)
+import argparse
+from collections import Iterable
+
+
+def create_cmd():
+    parser = argparse.ArgumentParser()
+    pass
+
+
+def create(db_dir: str,
+           orgs_table_path: str,
+           btax_class_profiles: Iterable,
+           btax_repr_profiles: Iterable,
+           btax_level=None,
+           num_threads=None,
+           config_path=None,
+           **kwargs):
+    pass
