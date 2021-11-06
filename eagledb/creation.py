fc11fcbb3bfeec9b1a316b00ef8b821cba802760
Denis <loven7doo@gmail.com>
2021-11-07 00:50:40 +0300

    eagledb genomes table correction

--- eagledb/creation.py
+++ eagledb/creation.py
@@ -3,18 +3,40 @@
 import argparse
 from collections import Iterable
 
+import pandas as pd
+
 
 def create_cmd():
     parser = argparse.ArgumentParser()
     pass
+    # TODO: genomes_table_path -> genomes_table
 
 
 def create(db_dir: str,
-           genomes_table_path: str,
+           genomes_table: pd.DataFrame,
            btax_class_profiles: Iterable,
            btax_repr_profiles: Iterable,
            btax_level=None,
            num_threads=None,
            config_path=None,
            **kwargs):
+    """
+
+    :param db_dir:
+    :param genomes_table:
+        id
+        name
+        taxonomy - fixed positions list of taxonomic units
+        profile_seqs - list of paths to FASTA with sequences used for btc or btr profile building
+                       sequences can be joined into single file or distributed between several files
+        genome - list of paths or links to the genome files (FASTA or archived FASTA)
+                 sequences can be joined into single file or distributed between several files
+    :param btax_class_profiles:
+    :param btax_repr_profiles:
+    :param btax_level:
+    :param num_threads:
+    :param config_path:
+    :param kwargs:
+    :return:
+    """
     pass
